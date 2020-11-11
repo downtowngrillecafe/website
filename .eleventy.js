437db013eaac6cf698a94d18eb5a3431e7f03c63
Tanner <dolb.tanner@gmail.com>
2020-11-10 16:49:24 -0800

    Create filter to handle allergens, comment out useless collections for now

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = config => {
+  /* TO BE REMOVED
+  * Custom Collections to be removed
   const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
   // Returns breakfast items, sorted by display order
   config.addCollection('breakfast', collection => {
@@ -12,12 +14,19 @@ module.exports = config => {
   config.addCollection('miscellaneous', collection => {
     return sortByDisplayOrder(collection.getFilteredByGlob('./src/miscellaneous/*.md'));
   });
+  */
+
+  // Create a filter to handle allergen tags
+  config.addFilter("check", function(allergen) {
+    return allergen ? allergen.slice(0, 1).toUpperCase() : false;
+  });
 
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
 
   // Set the directories to pass through to the 'dist' folder
   config.addPassthroughCopy('./src/images/');
+  config.addPassthroughCopy('./src/css/');
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
